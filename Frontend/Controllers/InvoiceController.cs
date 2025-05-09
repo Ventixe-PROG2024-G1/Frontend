7d778acc8efaee6b786e3bb5561dbb30bfd6df33
Emma <emmaolssondev@outlook.com>
2025-05-09 11:42:29 +0200

    fetch data from backend

--- Frontend/Controllers/InvoiceController.cs
+++ Frontend/Controllers/InvoiceController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Frontend.Models;
+using Frontend.Models.Invoice;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Controllers;
-
+[Route("[controller]")]
 public class InvoiceController : Controller
 {
+    private readonly HttpClient _https;
+
+    public InvoiceController(IHttpClientFactory httpFactory)
+    {
+        _https = httpFactory.CreateClient();
+    }
+
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet("GetInvoices")]
+    public async Task<IActionResult> GetInvoices()
+    {
+        var invoices = await _https
+            .GetFromJsonAsync<List<InvoiceDto>>("https://localhost:7042/api/Invoice");
+
+        return Json(invoices);
+    }
+
+    [HttpGet("GetById/{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var invoice = await _https
+            .GetFromJsonAsync<InvoiceDetailsDto>($"https://localhost:7042/api/Invoice/{id}");
+
+        if (invoice == null)
+            return NotFound();     
+
+        return Json(invoice);
     }
 }
---
+++ Frontend/Models/Invoice/InvoiceDetailsDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Models.Invoice;
+
+public class InvoiceDetailsDto
+{
+    
+    public DateTime CreatedDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public string Title { get; set; }
+    public int Price { get; set; }
+    public int Qty { get; set; }
+    public decimal Amount { get; set; }
+
+    public string CustomerName { get; set; }
+    public string CustomerAddress { get; set; }
+    public string CustomerPostalCode { get; set; }
+    public string CustomerCity { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomerPhone { get; set; }
+
+    public string EventName { get; set; }
+    public string EventAddress { get; set; }
+    public string EventCity { get; set; }
+    public string EventEmail { get; set; }
+    public string EventPhone { get; set; }
+
+}
