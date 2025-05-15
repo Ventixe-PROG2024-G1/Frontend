4757e8412004ed0cee66991128ea3392a83a1e3d
Emma <emmaolssondev@outlook.com>
2025-05-15 14:17:07 +0200

    update invoice css

--- Frontend/wwwroot/js/InvoiceDetails.js
+++ Frontend/wwwroot/js/InvoiceDetails.js
@@ -7,26 +7,44 @@
         })
         .then(invoice => {
             const div = document.getElementById('invoice-details');
+            console.log(invoice)
 
             div.innerHTML = `
-        <h3>Faktura: ${invoice.title}</h3>
-        <p><strong>Skapad:</strong> ${new Date(invoice.createdDate).toLocaleString('sv-SE')}</p>
-        <p><strong>Förfallodatum:</strong> ${new Date(invoice.dueDate).toLocaleDateString('sv-SE')}</p>
-        <p><strong>Pris:</strong> ${invoice.price} kr</p>
-        <p><strong>Antal:</strong> ${invoice.qty}</p>
-        <p><strong>Summa:</strong> ${invoice.amount.toFixed(2)} kr</p>
-        
-        <h4>Kund</h4>
-        <p><strong>Namn:</strong> ${invoice.customerName}</p>
-        <p><strong>Adress:</strong> ${invoice.customerAddress}, ${invoice.customerPostalCode} ${invoice.customerCity}</p>
-        <p><strong>E-post:</strong> ${invoice.customerEmail}</p>
-        <p><strong>Telefon:</strong> ${invoice.customerPhone}</p>
-        
-        <h4>Evenemang</h4>
-        <p><strong>Namn:</strong> ${invoice.eventName}</p>
-        <p><strong>Adress:</strong> ${invoice.eventAddress}, ${invoice.eventCity}</p>
-        <p><strong>E-post:</strong> ${invoice.eventEmail}</p>
-        <p><strong>Telefon:</strong> ${invoice.eventPhone}</p>
+            <div class="invoice-details-header">
+                <div>
+                    <h3>#INV10012</h3>
+                    <p class="invoice-list-status">UnPaid</p>
+                </div>
+                <div>
+                    <p>Issued Date ${new Date(invoice.createdDate).toLocaleString('sv-SE')}</p>
+                    <p>Due Date${new Date(invoice.dueDate).toLocaleDateString('sv-SE')}</p>
+                </div>
+            </div>
+            <div class="invoice-details-body">
+                <div>
+                <p>Bill from:</>
+                    <h4>${invoice.eventName}</h4>
+                    <p>${invoice.eventAddress}, ${invoice.eventCity}</p>
+                    <p>${invoice.eventEmail}</p>
+                    <p>${invoice.eventPhone}</p>
+                </div>
+                <div>
+                    <p>Bill to:</p>
+                    <h4>${invoice.customerName}</h4>
+                    <p>${invoice.customerAddress}, ${invoice.customerPostalCode} ${invoice.customerCity}</p>
+                    <p>${invoice.customerEmail}</p>
+                    <p>${invoice.customerPhone}</p>
+                </div>
+            </div>
+            <div class="invoice-details-ticets">
+            <h4>Ticket Details</h4>
+            <div>
+                <p><strong>Pris:</strong> ${invoice.price} kr</p>
+                <p><strong>Antal:</strong> ${invoice.qty}</p>
+            </div>
+                <p><strong>Summa:</strong> ${invoice.amount.toFixed(2)} kr</p>
+            </div>
+
       `;
         })
         .catch(err => {
