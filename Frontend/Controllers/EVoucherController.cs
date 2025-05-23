f4962e8f1deb73d971c2ac213071221edd6f6aed
Emma <emmaolssondev@outlook.com>
2025-05-23 13:34:25 +0200

    add evoucher fetch and js handeler

--- Frontend/Controllers/EVoucherController.cs
+++ Frontend/Controllers/EVoucherController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Frontend.Models.EVoucher;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Controllers
 {
+    [Route("[controller]")]
     public class EVoucherController : Controller
     {
+        private readonly HttpClient _https;
+
+        public EVoucherController(IHttpClientFactory httpFactory)
+        {
+            _https = httpFactory.CreateClient();
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet("GetByIds/{Invoiceid}/{Eventid}")]
+        public async Task<IActionResult> GetByIds(string Invoiceid, string Eventid)
+        {
+            var Evoucher = await _https.GetFromJsonAsync<EVoucherModel>($"https://localhost:7260/api/Evoucher/{Invoiceid}/{Eventid}");
+
+            if (Evoucher == null)
+                return NotFound();
+
+            return Json(Evoucher);
         }
     }
 }
---
+++ Frontend/Models/EVoucher/EVoucherModel.cs
@@ -0,0 +1,50 @@
+﻿namespace Frontend.Models.EVoucher;
+
+public class EVoucherModel
+{
+    public string Id { get; set; } = "";
+    public string TicketId { get; set; } = "";
+    public EVoucherTicket Ticket { get; set; } = new();
+    public EventSchedule Schedule { get; set; } = new();
+    public ProhibitedItems ProhibitedItems { get; set; } = new();
+    public TermsConditions TermsConditions { get; set; } = new();
+    public VenueMap Map { get; set; } = new();
+}
+
+public class EVoucherTicket
+{
+    public string Id { get; set; } = "";
+    public string Title { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Type { get; set; } = "";
+    public string InvoiceID { get; set; } = "";
+    public string SeatNumber { get; set; } = "";
+    public string Gate { get; set; } = "";
+    public string Location { get; set; } = "";
+    public string Date { get; set; } = "";
+    public string Time { get; set; } = "";
+}
+
+public class EventSchedule
+{
+    public string EventName { get; set; } = "";
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+}
+
+public class ProhibitedItems
+{
+    public int Id { get; set; }
+    public List<string> Items { get; set; } = new();
+}
+
+public class TermsConditions
+{
+    public string Text { get; set; } = "";
+}
+
+public class VenueMap
+{
+    public int Id { get; set; }
+    public string Url { get; set; } = "";
+}
