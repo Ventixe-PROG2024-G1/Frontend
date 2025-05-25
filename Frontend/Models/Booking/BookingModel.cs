7b053114be404e5e0248306860995b3978dedba0
Limpan89 <lbroback89@gmail.com>
2025-05-25 11:25:09 +0200

    initial commit

---
+++ Frontend/Models/Booking/BookingModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Frontend.Models.Booking;
+
+public class BookingModel
+{
+    public string Id { get; set; } = null!;
+    public DateTime? Created { get; set; }
+    public int TicketQuantity { get; set; }
+    public decimal TotalTicketPrice { get; set; }
+    public string Status { get; set; } = null!;
+
+    public decimal TicketPrice { get; set; }
+    public string? TicketCategory { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? EventName { get; set; }
+    public string? EventCategory { get; set; }
+
+    public string UserId { get; set; } = null!;
+    public string EventId { get; set; } = null!;
+    public string TicketId { get; set; } = null!;
+    public string? InvoiceId { get; set; }
+    public string? VoucherId { get; set; }
+}
---
+++ Frontend/Models/Booking/BookingQueryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Frontend.Models.Booking;
+
+public class BookingQueryResponse
+{
+    public IEnumerable<BookingModel> Bookings { get; set; } = null!;
+    public int CurrentPage { get; set; } = 1;
+    public int TotalPages { get; set; }
+}
