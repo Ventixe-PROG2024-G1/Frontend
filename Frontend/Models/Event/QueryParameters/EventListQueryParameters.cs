a70c0f52aa46163182225ccc2a156af674abedb8
RamiObaid92 <163041066+RamiObaid92@users.noreply.github.com>
2025-05-27 13:32:15 +0200

    Query Params model

---
+++ Frontend/Models/Event/QueryParameters/EventListQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace Frontend.Models.Event.QueryParameters;
+
+public class EventListQueryParameters
+{
+    public int PageNumber { get; set; } = 1;
+    public int? PageSize { get; set; }
+    public string? CategoryIdFilter { get; set; }
+    public string? StatusFilter { get; set; }
+    public string? SearchTerm { get; set; }
+    public string? DateFilter { get; set; }
+}
