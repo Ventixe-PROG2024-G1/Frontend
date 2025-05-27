048b915f436bda32660f017e58ae355082aeaa8e
Limpan89 <lbroback89@gmail.com>
2025-05-27 21:01:04 +0200

    Initial commit mode

---
+++ Frontend/Models/Booking/AddBookingDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Models.Booking;
+
+public class AddBookingDto
+{
+    public int TicketQuantity { get; set; }
+    public decimal TicketPrice { get; set; }
+    public string UserName { get; set; } = null!;
+    public string UserEmail { get; set; } = null!;
+    public string UserPhone { get; set; } = null!;
+    public string UserStreetAddress { get; set; } = null!;
+    public string UserPostalCode { get; set; } = null!;
+    public string UserCity { get; set; } = null!;
+    public string UserId { get; set; } = null!;
+    public string EventId { get; set; } = null!;
+    public string TicketId { get; set; } = null!;
+}
---
+++ Frontend/Models/EventDetails/TicketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Frontend.Models.EventDetails;
+
+public class TicketModel
+{
+    public string Id { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string Tiers { get; set; } = null!;
+    public string? TierDescription { get; set; }
+}
